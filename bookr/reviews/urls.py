e0e883d477825de609774dcfc0b6c70c8bb1046a
dev-com2020 <kwp@tlen.pl>
2023-04-29 13:49:43 +0200

    29.04 13:49

--- bookr/reviews/urls.py
+++ bookr/reviews/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.welcome_view, name='welcome'),
+    path('books/', views.book_list, name='book_list'),
 
 ]
--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from .models import Book
+from .utils import average_rating
 
 
 # Create your views here.
@@ -13,3 +14,36 @@ def welcome_view(request):
     message = f"<html><h1>Welcome to Bookr!</h1></html> " \
               f"<p>{Book.objects.count()} książek w bazie danych</p></html>"
     return HttpResponse(message)
+
+def book_list(request):
+    global context
+    books = Book.objects.all()
+    book_list = []
+    for book in books:
+        reviews = book.review_set.all()
+        if reviews:
+            book_rating = average_rating([review.rating for review in reviews])
+            number_of_reviews = len(reviews)
+        else:
+            book_rating = None
+            number_of_reviews = 0
+        book_list.append({
+            'book': book,
+            'book_rating': book_rating,
+            'number_of_reviews': number_of_reviews})
+        context = {'book_list': book_list}
+    return render(request, "book_list.html", context)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
