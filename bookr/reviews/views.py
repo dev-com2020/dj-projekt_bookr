c7c7b8019acb0fc10c6d73c5d926cc28bcd59e27
dev-com2020 <kwp@tlen.pl>
2023-04-29 10:55:40 +0200

    29.04 10:55

--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    name = request.GET.get('name') or 'world'
-    return render(request, "base.html")
+    name = "Bookr"
+    return render(request, "base.html", {"name": name})
